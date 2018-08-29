935e97d862ef07a2343b9022ac8d33924184f67d
khuxkm fbexl <khuxkm@ttm.sh>
2018-08-29 05:58:59 -0400

    Fix adding tasks

--- app.py
+++ app.py
@@ -3,22 +3,22 @@ import os.path as fs
 
 class Application(tkinter.Frame):
 	def __init__(self,parent=None):
-		print("init app frame")
+#		print("init app frame")
 		super(Application,self).__init__(parent)
 		self.parent = parent
 		self.pack()
 		self.create_widgets()
 
 	def create_widgets(self):
-		print("entry widget")
+#		print("entry widget")
 		self.entry = tkinter.Entry(self)
 		self.entry.pack()
-		print("listbox widget")
+#		print("listbox widget")
 		self.list = tkinter.Listbox(self)
-		print("call loadTasks")
+#		print("call loadTasks")
 		self.loadTasks()
 		self.list.pack()
-		print("add/resolve buttons")
+#		print("add/resolve buttons")
 		self.button_frame = tkinter.Frame(self)
 		self.button_frame.pack(side="bottom")
 		self.add_task_button = tkinter.Button(self.button_frame,text="Add",command=self.add_task)
@@ -28,22 +28,24 @@ class Application(tkinter.Frame):
 
 	def loadTasks(self):
 		if fs.exists(fs.expanduser("~/.tototasks")):
-			print("tasks file exists, open it")
+#			print("tasks file exists, open it")
 			with open(fs.expanduser("~/.tototasks")) as f:
 				self.tasks = json.load(f)["tasks"]
-				print("{!r}".format(self.tasks))
+#				print("{!r}".format(self.tasks))
 				for entry in self.tasks:
 					self.list.insert("end",entry)
 		else:
-			print("no tasks file!")
+#			print("no tasks file!")
 			self.tasks = []
 
 	def add_task(self,*args,**kwargs):
-		print("add_task "+self.entry.get())
+#		print("add_task "+self.entry.get())
 		self.tasks.append(self.entry.get())
-#		self.list.insert("end",self.tasks[-1])
 		with open(fs.expanduser("~/.tototasks"),"w") as f:
 			json.dump(dict(tasks=self.tasks),f)
+		self.list.delete(0,"end")
+		for entry in self.tasks:
+			self.list.insert("end",entry)
 
 	def resolve(self,*args,**kwargs):
 		print("resolve")
